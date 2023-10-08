b9581ac685336aa8df6f53564b77ff1ddd5c4016
Om Kumar Kaushik <omkumar.kaushik@gmail>
2023-10-08 17:04:46 +0530

    New class added

--- Student.java
+++ Student.java
@@ -1,7 +1,10 @@
+import Teachers.java;
+
 class Student{
     public static void main(String[] args){
         System.out.println("This is student class main function");
         System.out.println("Yupzz");
-
+        Teachers teach = new Teachers();
+        teach.sayHi();
     }
 }
---
+++ Teachers.java
@@ -0,0 +1,10 @@
+class Teachers{
+
+    public Teachers(){
+        System.out.println(x:"Creating Teachers object!!");
+    }
+
+    public void sayHi(){
+        System.out.println("Hi!! From Teachers");
+    }
+}
